263b92acb7bba607a0015b08c6a028821c1b84db
¨ZiskaS¨ <franziska.stude@gmx.de>
2023-05-22 13:38:54 +0200

    add activity one

--- activity/1_session/aboutme.js
+++
@@ -1,8 +0,0 @@
-let fullName = "Franziska Stude";
-let currentProfession = "Customer Relationship Manager";
-let newRole = "Full Stack Developer";
-
-let phrase = `Hola, me llamo ${fullName} y me dedico a ${currentProfession}. 
-Estoy cursando este Máster porque me gustaría trabajar en ${newRole}}.`;
-
-console.log(phrase);
